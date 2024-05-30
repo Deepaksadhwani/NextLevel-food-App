a30b4b64dedf323b792e4fba9682493a40a59198
Deepak sadhwani <108820079+Deepaksadhwani@users.noreply.github.com>
2024-05-30 23:13:38 +0530

    form submission with server actions

--- app/meals/share/image-picker.js
+++ app/meals/share/image-picker.js
@@ -1,24 +1,55 @@
-'use client'
-import { useRef } from "react";
+"use client";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 export default function ImagePicker({ label, name }) {
   const imageInputRef = useRef();
+  const [pickedImage, setPickedImage] = useState(null);
   const handlePicClick = () => {
-      imageInputRef.current.click();
+    imageInputRef.current.click();
+  };
+
+  function handleImageChange(event) {
+    const file = event.target.files[0];
+    console.log(file)
+    if (!file) {
+      return;
+    }
+    const fileReader = new FileReader();
+
+    // this function will be trigged once DataUrl (readAsDataUrl) method is done
+    fileReader.onload = () => {
+      // to access generated data url
+      setPickedImage(fileReader.result);
+    };
+
+    //this does not return any value or promise so we use onLoad
+    fileReader.readAsDataURL(file);
   }
   return (
     <div className={classes.picker}>
       <label htmlFor="image">{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {pickedImage && (
+            <Image src={pickedImage} fill alt="The image is select by the user." />
+          )}
+          {!pickedImage && <p>No image is picked yet.</p>}
+        </div>
         <input
-            ref={imageInputRef} 
-            className={classes.input}
+          ref={imageInputRef}
+          className={classes.input}
           type="file"
           id="image"
           accept="image/png, image/jpeg"
           name={name}
+          onChange={handleImageChange}
         />
-        <button onClick={handlePicClick} className={classes.button} type="button">
+        <button
+          onClick={handlePicClick}
+          className={classes.button}
+          type="button"
+        >
           Pick an Image
         </button>
       </div>
